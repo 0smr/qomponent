2d1ada5ff2e8a85462058cc8ae386ac3509bb36a
SMR <seyyedmortezarazavi76@gmail.com>
2022-12-30 16:15:13 +0330

    Add some javascript helper functions.

--- Qomponent/Qomponent.qml
+++ Qomponent/Qomponent.qml
@@ -9,10 +9,31 @@ import QtQuick 2.15
 Item {
     id: root
 
-    function alpha(c, a) {
-        return Qt.rgba(c.r,c.g,c.b,a);
+    /**
+     * @method alpha
+     * @param color input font
+     * @param a new alpha value
+     * @returns Input color with new alpha value.
+     */
+    function alpha(color: color, a: real): color {
+        return Qt.rgba(color.r, color.g, color.b, a);
     }
 
+    /**
+     * @method font
+     * @param ifont input font
+     * @param properties new properties
+     * @returns Input font with new properties.
+     */
+    function font(ifont: font, prop): font {
+        return Qt.font(Object.assign({}, ifont, prop));
+    }
+
+    /**
+     * @method copy
+     * Copy input text to clipboard.
+     * @param text
+     */
     function copy(text) {
         dummytedit.text = text;
         dummytedit.selectAll();
--- Qomponent/scripts.js
+++ Qomponent/scripts.js
@@ -1,3 +1,37 @@
 // Copyright (C) 2022 smr.
 // SPDX-License-Identifier: MIT
 // https://smr76.github.io
+.pragma library
+
+/**
+ * @abstract
+ *  Input properties are evaluated and the final value of current object is returned.
+ * @param {String|Array<String>} properties properties chain as String or Array
+ * @param {*} _default
+ * @returns
+ *  Return the final property in the properties chain, or default if the value is undefined.
+ */
+const _qget = function(properties, _default = undefined) {
+    const keys = [] instanceof Array ? properties : properties.split(/[\[\]\.]/).filter(x => x);
+    let result = this;
+    for(const key of keys) {
+        if(result[key] === undefined) return _default;
+        else result = result[key];
+    }
+    return result;
+}
+
+/**
+ * @abstract
+ *  Clamp the Number value to min and max.
+ * @param {Number} min
+ * @param {Number} max
+ * @returns Return a value between min and max.
+ */
+const _qclamp = function(min, max) {
+    return Math.max(min, Math.min(this, max));
+}
+
+Array.prototype.qget = _qget;
+Object.prototype.qget = _qget;
+Number.prototype.qclamp = _qclamp;
