ee0f8f24337818265e2524e89828dc05718ce4c4
Parisa <parisahamed76@gmail.com>
2022-12-31 18:34:41 +0330

    update dashed-lines.glsl

--- Qomponent/shader/dashed-lines.glsl
+++ Qomponent/shader/dashed-lines.glsl
@@ -1,3 +1,7 @@
+#ifdef GL_ES
+    precision highp float;
+    precision highp int;
+#endif
 uniform highp float qt_Opacity;
 varying highp vec2 qt_TexCoord0;
 uniform highp vec4 color;
@@ -10,13 +14,17 @@ uniform highp float lineHeight;
 uniform highp float xoffset;
 uniform highp vec2 minor;
 uniform highp vec2 major;
+
+
 void main() {
+
     vec2 uv = (qt_TexCoord0 + vec2(xoffset/width - .5, 0.)) * vec2(width/spacing, 1.);
     vec2 gs = vec2(100./spacing/width, 1./height);
     vec2 mainGrid = fract(uv);
     vec2 minorGrid = fract(uv / vec2(minor.x, 1));
     vec2 majorGrid = fract(uv / vec2(major.x, 1));
-    float ruler = mix(smoothstep(0., gs.y, mainGrid.y + minor.y - 1.), smoothstep(0., gs.y, minorGrid.y + lineHeight - 1),
+
+    float ruler = mix(smoothstep(0., gs.y, mainGrid.y + minor.y - 1.), smoothstep(0., gs.y, minorGrid.y + lineHeight - 1.0),
                       smoothstep(0., gs.x/minor.x, minorGrid.x - lineWidth/spacing/minor.x));
     gl_FragColor = mix(originColor, color, min(abs(floor(uv.x)), 1.));
     gl_FragColor *= (1. - smoothstep(0., gs.x, mainGrid.x - lineWidth/spacing * 0.99));
